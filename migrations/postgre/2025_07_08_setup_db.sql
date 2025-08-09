436fab4706aa3cb9224f5dcad8220c544e9f73e5
Halludbaa <rinfaabdullah@gmail.com>
2025-08-09 09:53:02 +0700

    fix/product-categories-migration

--- migrations/postgre/2025_07_08_setup_db.sql
+++ migrations/postgre/2025_07_08_setup_db.sql
@@ -44,6 +44,18 @@ CREATE TABLE IF NOT EXISTS branch_products (
     CONSTRAINT branch_id_fk FOREIGN KEY(branch_id) REFERENCES branches(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
+CREATE TABLE IF NOT EXISTS categories (
+    id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    name VARCHAR(100) NOT NULL,
+);
+
+CREATE TABLE IF NOT EXISTS product_categories (
+    product_id uuid NOT NULL,
+    category_id uuid NOT NULL,
+    CONSTRAINT product_id_fk FOREIGN KEY(product_id) REFERENCES products(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT category_id_fk FOREIGN KEY(category_id) REFERENCES categories(id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
 
 
 -- NEXT ORDER SCHEMA in mongo
